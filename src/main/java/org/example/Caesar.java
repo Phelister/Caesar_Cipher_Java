2e01fd47b14d3ad3ddc86b7316aed5e7b448d852
pbogonko <pbogonko@safaricom.co.ke>
2023-04-24 10:51:12 +0300

    implemented getters, setters, all args constructor and no args constructor

--- src/main/java/org/example/Caesar.java
+++ src/main/java/org/example/Caesar.java
@@ -1,14 +1,46 @@
 package org.example;
 
 public class Caesar {
+    private String name;
+    private int key;
+
+    public Caesar(){
+        this.name=null;
+        this.key =0;
+    }
+
+    public Caesar(String name,int key){
+        this.name=name;
+        this.key =key;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getKey(){
+        return this.key;
+    }
+    public String setName(String name){
+        return this.name=name;
+    }
+    public int setKey(int key){
+        return this.key=key;
+    }
 
 
-    public String encoder (String input){
+    public String encrypt (String input, int key){
+        if(input ==null || input.length()==0){
+            return "Error: Please provide a valid input to proceed encrypting the String";
+        }
 
     }
 
 
-    public String decoder (String input){
+    public String decrypt (String input, int key){
+        if(input ==null || input.length()==0){
+            return "Error: Please provide a valid input to proceed decrypting the String";
+        }
 
     }
 }
