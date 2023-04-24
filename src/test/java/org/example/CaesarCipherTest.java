3da0eabccc8e1b4151b0134bc00e8871e8b3fc22
pbogonko <pbogonko@safaricom.co.ke>
2023-04-24 16:01:46 +0300

    editted tests

--- src/test/java/org/example/CaesarCipherTest.java
+++ src/test/java/org/example/CaesarCipherTest.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @DisplayName(value = "Caesar cipher related Tests")
 class CaesarCipherTest {
+
+//
+//    @BeforeAll
+//    void eachSetUp(){
+//
+//    }
 
     @Test
     void getName() {
@@ -24,6 +33,20 @@ class CaesarCipherTest {
     }
     @Test
     @DisplayName(value = "Check decryption and encryption")
-    void encryptDecrypt() {
+    String encryptDecrypt(String input,int key) {
+       // assertTrue();
+        // assertFalse();
+        return "JK";
     }
+
+
+    @Test
+    @DisplayName(value = "Confirm encryption and decryption works well")
+    void confirmEncryptDecrypt() {
+        String input="HI";
+        int key =2;
+        assertEquals("JK",encryptDecrypt(input,key));
+    }
+
+
 }
