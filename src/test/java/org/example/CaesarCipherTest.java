31b5dcaa43022df95a6ccb09ba8f8a74f41ed013
pbogonko <pbogonko@safaricom.co.ke>
2023-04-24 15:30:40 +0300

    added test

---
+++ src/test/java/org/example/CaesarCipherTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+
+class CaesarCipherTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getKey() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setKey() {
+    }
+
+    @Test
+    void encryptDecrypt() {
+    }
+}
